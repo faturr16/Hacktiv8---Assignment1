68807d713d32c2786bcc25735ae0ccc68f9429b3
faturr16 <113949591+faturr16@users.noreply.github.com>
2022-09-22 06:08:06 +0700

    On branch master  Your branch is up to date with 'testProject/master'.
        Changes to be committed:
    	modified:   main.go
    	modified:   project.exe

--- main.go
+++ main.go
@@ -6,9 +6,22 @@ import (
 
 func main() {
 
-	fmt.Println("Your Name :")
 	var nama string
+	fmt.Println("Nama Kamu:")
 	fmt.Scanln(&nama)
 
-	fmt.Println("Your Nama is : ", nama)
+	var umur int
+	fmt.Println("Umur Kamu:")
+	fmt.Scanln(&umur)
+
+	fmt.Println("Nama kamu adalah: ", nama)
+	fmt.Println("Umur kamu:", umur)
+
+	if umur < 30 {
+		fmt.Println("Kamu masih muda", nama, "Semangat terus ya :)")
+	} else if umur <= 50 {
+		fmt.Println("Kamu sudah dewasa", nama, "Saat nya berprilaku seperti orang dewasa")
+	} else {
+		fmt.Println("Kamu sudah lebih dewasa", nama, "Kamu harus mencari kesenagan sendiri")
+	}
 }
