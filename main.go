ec68018cfda4216e7f2b4da3e201e7a6a002c3ea
faturr16 <113949591+faturr16@users.noreply.github.com>
2022-09-25 17:03:44 +0700

    Changes to be committed: 	modified:   main.go 	modified:   project.exe

--- main.go
+++ main.go
@@ -2,26 +2,102 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
+
+type student struct {
+	nama, alamat, pekerjaan, alasan string
+}
 
 func main() {
 
-	var nama string
-	fmt.Println("Nama Kamu:")
-	fmt.Scanln(&nama)
+	Student := []student{
+		{
+			nama:      "Nama: Fatur\n",
+			alamat:    "Alamat: Depok\n",
+			pekerjaan: "Pekerjaan: Mahasiswa\n",
+			alasan:    "Alasan memilih golang:ingin menambah ilmu dan mendalami tentang backend\n",
+		},
 
-	var umur int
-	fmt.Println("Umur Kamu:")
-	fmt.Scanln(&umur)
+		{
+			nama:      "Nama: Alex\n",
+			alamat:    "Alamat: Demak\n",
+			pekerjaan: "Pekerjaan: Akunta\n",
+			alasan:    "Alasan memilih golang: bahasa yang ringan untuk laptop kentang \n",
+		},
 
-	fmt.Println("Nama kamu adalah: ", nama)
-	fmt.Println("Umur kamu:", umur)
+		{
+			nama:      "Nama: RObin\n",
+			alamat:    "Alamat: Surabaya\n",
+			pekerjaan: "Pekerjaan: sales\n",
+			alasan:    "Alasan: Keren aja\n",
+		},
 
-	if umur < 30 {
-		fmt.Println("Kamu masih muda", nama, "Semangat terus ya :)")
-	} else if umur <= 50 {
-		fmt.Println("Kamu sudah dewasa", nama, "Saat nya berprilaku seperti orang dewasa")
-	} else {
-		fmt.Println("Kamu sudah lebih dewasa", nama, "Kamu harus mencari kesenagan sendiri")
+		{
+			nama:      "Nama: Josh\n",
+			alamat:    "Alamat: Bogor\n",
+			pekerjaan: "Pekerjaan: Mahasiswa\n",
+			alasan:    "Alasan: mau nyaingin bjorka\n",
+		},
+
+		{
+			nama:      "Nama: George\n",
+			alamat:    "Alamat: Bogor\n",
+			pekerjaan: "Pekerjaan: Chef\n",
+			alasan:    "Alasan memilih golang: bahasa pemrogaraman yang ringan \n",
+		},
+
+		{
+			nama:      "Nama: Marvin\n",
+			alamat:    "Alamat: Cipete\n",
+			pekerjaan: "Pekerjaan: Mahasiswa\n",
+			alasan:    "Alasan memilih golang: bahasa yang ringan untuk laptop kentang \n",
+		},
+
+		{
+			nama:      "Nama: Kevin\n",
+			alamat:    "Alamat: Alaska\n",
+			pekerjaan: "Pekerjaan: Mahasiswa\n",
+			alasan:    "Alasan: mau nyaingin bjorka\n",
+		},
+
+		{
+			nama:      "Nama: Edward\n",
+			alamat:    "Alamat: California\n",
+			pekerjaan: "Pekerjaan: Sales\n",
+			alasan:    "Alasan memilih golang: Ingin mendalamin tentang Golang \n",
+		},
+
+		{
+			nama:      "Nama: Thomas\n",
+			alamat:    "Alamat: Kemang\n",
+			pekerjaan: "Pekerjaan:Polisi \n",
+			alasan:    "Alasan memilih golang: biar keliatan bisa ngoding \n",
+		},
+
+		{
+			nama:      "Nama: Aswin\n",
+			alamat:    "Alamat: Bogor\n",
+			pekerjaan: "Pekerjaan: Chef\n",
+			alasan:    "Alasan memilih golang: mau nyaigin bjorka \n",
+		},
+
+		{
+			nama:      "Nama: Dion\n",
+			alamat:    "Alamat: Depok\n",
+			pekerjaan: "Pekerjaan: Mahasiswa\n",
+			alasan:    "Alasan memilih golang: Keren dan Ringan \n",
+		},
 	}
+
+	absen := os.Args[1]
+	for index, value := range Student {
+		indexString := strconv.Itoa(index)
+		if indexString == absen {
+			fmt.Println(value)
+		}
+
+	}
+
 }
